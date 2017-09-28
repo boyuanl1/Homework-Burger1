c0d1e67e1531d929908a409644e6bbb50fb5d9cd
boyuanl1 <boyuanl1@icloud.com>
2017-09-27 20:37:40 -0700

    final

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Cheese Burger');
+INSERT INTO cats (name) VALUES ('Double Cheese Burger');
+INSERT INTO cats (name, sleepy) VALUES ('Triple Cheese Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Big Mac', true);
+INSERT INTO cats (name, sleepy) VALUES ('Squid Burger', true);
+INSERT INTO cats (name) VALUES ('Pickle Burger');
